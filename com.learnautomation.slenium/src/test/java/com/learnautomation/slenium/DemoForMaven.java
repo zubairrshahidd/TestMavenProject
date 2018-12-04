3d0cb25bd45a0a07614ba3156ea43519a3f4cd69
xubr <xubr@DESKTOP-6G4HCK3.localdomain>
2018-12-04 16:08:48 +0500

    1st commit

---
+++ com.learnautomation.slenium/src/test/java/com/learnautomation/slenium/DemoForMaven.java
@@ -0,0 +1,41 @@
+package com.learnautomation.slenium;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class DemoForMaven {
+
+	@Test
+	public void testHello() {
+	System.out.println("Welcome to Maven");
+	
+	}
+	
+	@Test
+	public void Sum(){
+		System.out.println("SUM");
+		int a = 10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void Sub(){
+		System.out.println("SUB");
+		int a = 10;
+		int b=20;
+		Assert.assertEquals(10, b-a);
+	}
+
+	@Test
+	public void DIV(){
+		System.out.println("DIV");
+		int a = 10;
+		int b=20;
+		Assert.assertEquals(2, b/a);
+	}
+	
+}
